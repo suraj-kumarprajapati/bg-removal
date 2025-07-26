7173471cb25a1525b64f3cda323a8e53ee594404
Suraj Prajapati <sp0640481@gmail.com>
2025-07-26 23:13:10 +0530

    added clerk webhooks

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/dbConnect.js';
+import userRouter from './routes/userRoute.js';
 
 // port no
 const PORT = process.env.PORT || 5000;
@@ -16,11 +17,14 @@ app.use(express.json());
 app.use(cors());
 
 
+// API Routes
 app.get("/", (req, res) => {
     res.status(200).json({
         message : 'welcome to bg-removal',
     });
 });
+
+app.use("/api/user", userRouter);
 
 
 
